2c85f97c7ce1d8f5f9373033619a9ff0a863031d
Justin Barfield <justin.kyle.barfield@gmail.com>
2021-03-11 13:53:04 -0500

    Card has been adjusted to show an input field or translation based off the props 'on'. This will be used to determine if the card should be used as a test flashcard or review.

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -7,7 +7,14 @@ function Card(props) {
             <div className="card text-center">
                 <div className="card-body">
                     <h5 className="card-title">{props.character}</h5>
-                    <p className="card-text">{props.translation}</p>
+                    {props.on ? (
+                        <>
+                        <input type="text"></input>
+                        </>
+                    ) : (
+                        
+                        <p className="card-text">{props.translation}</p>
+                    )}
                 </div>
             </div>
         </>
