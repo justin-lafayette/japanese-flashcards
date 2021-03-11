e4bf023db49347c0782822a99adf28ae1a100c8d
Justin Barfield <justin.kyle.barfield@gmail.com>
2021-03-11 14:02:53 -0500

    Functionality to show all cards from imported json moved to notes.js. Issue to properly view cards in correct order could not be resolved at this time. Cards would need to be alligned top-bottom and right-left based off the character group they are with. Functionality is feasable but existing solution requires refinement.
    States added to monitor random hir, kat, acc, and kanji. Other states to monitor flash cards and adjust their display have also been added. importStates has been removed and functionality added to importAndShuffleArray to reduce the number of page renders before complete page load. Functionality in importAndShuffleArray added to use the Fisher-Yates shuffle in order to randomly assign imported objects. Further refinement in the future will be neccessary as the random shuffle will need to be used by other functions.
    
    Other placeholder functions added before render as a reminder of some items that will need to be addressed.
    
    Temporary button added to page to test functionality of looping cards. nextCard function may possibly be migrated to entryValidation if possible.

--- client/src/pages/Review.js
+++ client/src/pages/Review.js
@@ -3,6 +3,14 @@ import Container from '../components/Container';
 import Card from '../components/Card';
 
 import jCharacters from '../jCharacters.json';
+import jCharactersHir from '../jCharacters-hir.json';
+import jCharactersHirDak from '../jCharacters-hir-dak.json';
+import jCharactersHirCombo from '../jCharacters-hir-combo.json';
+import jCharactersKat from '../jCharacters-kat.json';
+import jCharactersKatDak from '../jCharacters-kat-dak.json';
+import jCharactersKatCombo from '../jCharacters-kat-combo.json';
+import jCharactersSmallLetters from '../jCharacters-smallLetters.json';
+import jCharactersSymbols from '../jCharacters-symbols.json';
 
 // TODO:
 // render data
@@ -12,11 +20,109 @@ import jCharacters from '../jCharacters.json';
 class Review extends Component {
 
     state={
+        hiragana: [],
+        katakana: [],
+        accents: [],
+        kanji: [],
+        rHiragana: [],
+        rKatakana: [],
+        rAccents: [],
+        rKanji: [],
+        correctFlash:[],
+        wrongFlash: [],
+        flashCardIndex: 0,
+        flashCardSelections: [""],
+        flashCardsOn: true
+    };
+
+    // importStates = ()=> {
+    //     this.setState({
+    //         hiragana: [jCharactersHir, jCharactersHirDak, jCharactersHirCombo],
+    //         katakana: [jCharactersKat, jCharactersKatDak, jCharactersKatCombo],
+    //         accents: [jCharactersSmallLetters, jCharactersSymbols]
+    //     }, ()=> this.shuffleArray());
+    // };
+
+    importAndShuffleArray = ()=> {
+        // Solution found here using the Fisher-Yates shuffle. Has been adapted to use ES6 and custom variables: https://javascript.info/task/shuffle 
+
+        // import to variables done here to prevent multiple page re-rendering before displaying
+        let hir = [jCharactersHir, jCharactersHirDak, jCharactersHirCombo];
+        let kat = [jCharactersKat, jCharactersKatDak, jCharactersKatCombo];
+        let acc = [jCharactersSmallLetters, jCharactersSymbols];
+
+        let rHir = hir;
+        let rKat = kat;
+        let rAcc = acc;
+
+        rHir.map((value, index) => {
+            let currentIndex = rHir[index];
+
+            currentIndex.map( (val, ind) => {
+
+                let random = Math.floor(Math.random() * (ind + 1));
+                [currentIndex[ind], currentIndex[random]] = [currentIndex[random], currentIndex[ind]];
+            });
+        });
+
+        rKat.map((value, index) => {
+            let currentIndex = rKat[index];
+
+            currentIndex.map( (val, ind) => {
+
+                let random = Math.floor(Math.random() * (ind + 1));
+                [currentIndex[ind], currentIndex[random]] = [currentIndex[random], currentIndex[ind]];
+            });
+        });
+
+        rAcc.map((value, index) => {
+            let currentIndex = rAcc[index];
+
+            currentIndex.map( (val, ind) => {
+
+                let random = Math.floor(Math.random() * (ind + 1));
+                [currentIndex[ind], currentIndex[random]] = [currentIndex[random], currentIndex[ind]];
+            });
+        });
+
+        this.setState({
+            hiragana: hir,
+            katakana: kat,
+            accents: acc,
+            rHiragana: rHir,
+            rKatakana: rKat,
+            rAccents: rAcc,
+            flashCardSelections: rHir[0]
+        }, ()=> console.log("Random Hir: ", this.state.flashCardSelections));
+    };
+
+    entryValidation = ()=> {
 
     };
 
+    nextCard = (e)=> {
+        e.preventDefault();
+
+        let count = this.state.flashCardIndex;
+        count++;
+
+        this.setState({
+            flashCardIndex: count
+        })
+    };
+
+    inputReviewSelection = ()=> {
+
+    };
+
+    componentDidMount() {
+        this.importAndShuffleArray();
+    }
+
     render() {
-        console.log(jCharacters[0])
+        let fCS= this.state.flashCardSelections;
+        let fCI= this.state.flashCardIndex;
+
         return (
             <div className="tab-pane fade show active" id="review" role="tabpanel" aria-labelledby="review-tab">
                 <Container
@@ -24,27 +130,23 @@ class Review extends Component {
                 >
                     <Container>
 
-                        <div className="row">Hiragana</div>
+                        <div className="row">Flashcards</div>
 
-                        <div className="row row-cols-lg-5 row-cols-sm-2 g-0">
-
-                            {jCharacters.map( (value, index) => (
-
-                                // {jCharacters.alphabet = "hir" ?}
-
-                                <div className="col">
-                                        <Card 
-                                            key={index}
-                                            character={value.character}
-                                            translation={value.englishTranslation}
-                                            altTranslation={value.alternateEnglishTranslation}
-                                            isLetter={value.isLetter}
-                                            />
-                                </div>
-                            ))}
-
+                        <div className="row">
+                            {/* Write selected items to single array in flashCardSelections
+                                User flashCardIndex to loop through array until complete
+                                Set wrong and right answers while looping
+                                evaluate after complete & offer review of wrong
+                            */}
+                            
+                            <Card 
+                                character={fCS[fCI].character}
+                                translation={fCS[fCI].englishTranslation}
+                                on={this.state.flashCardsOn}
+                                />
                         </div>
-                        <div className="row">Katakana</div>
+
+                        <button onClick={this.nextCard}>Next Card</button>
 
                     </Container>
 
